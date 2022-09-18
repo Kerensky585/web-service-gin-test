7990b9c1cb81ec67c28a8f947137b04d312f661a
Kerensky585 <37952431+Kerensky585@users.noreply.github.com>
2022-09-18 12:11:48 +0100

    Inital upload to syn the repo.

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/Kerensky585/walletapi"
+)
+
+// TODO this  would not be required in a library module!!!! used for testing here to init endpoints, open the DB connection using settings.
+func main() {
+
+	if walletapi.InitEndPoints(){
+
+		if DbConnect("appsettings.json")
+
+		walletapi.Router.Run("localhost:8080")	
+
+	}		
+}
