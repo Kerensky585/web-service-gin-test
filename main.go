5a745009a45651d7667d724aa1f6b94cb2be49a9
Kerensky585 <37952431+Kerensky585@users.noreply.github.com>
2022-09-18 18:41:19 +0100

    Adding some basic test data in a DB file, made sure correct repo and tag for appi is used.

--- main.go
+++ main.go
@@ -7,11 +7,10 @@ import (
 // TODO this  would not be required in a library module!!!! used for testing here to init endpoints, open the DB connection using settings.
 func main() {
 
-	if walletapi.InitEndPoints(){
+	if walletapi.InitEndPoints() {
 
-		if DbConnect("appsettings.json")
-
-		walletapi.Router.Run("localhost:8080")	
-
-	}		
+		if walletapi.DbController("appsettings.json") {
+			walletapi.Router.Run("localhost:8080")
+		}
+	}
 }
